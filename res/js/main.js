4beb939716626eaf3f2c3d657c5046c87dae5aeb
Lukáš Hodovník <114694381+lukas02727@users.noreply.github.com>
2022-10-31 10:22:30 +0100

    Add files via upload

---
+++ res/js/main.js
@@ -0,0 +1,9 @@
+const ahojj = document.getElementById("ahoj");
+const buttono = document.getElementById("button");
+const inputo = document.getElementById("velikost");
+
+buttono.onclick = () => {
+    ahojj.style.fontSize = inputo.value+"px";
+}
+
+
